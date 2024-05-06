434d63e1e28db7bf6305a786e648b28112b6b19b
doya986 <169078407+doya986@users.noreply.github.com>
2024-05-06 20:35:19 +0300

    Delete homepage.js

--- homepage.js
+++
@@ -1,9 +0,0 @@
-function pop(msg, myYes) {
-    var confirmBox = $("#confirm");
-    confirmBox.find(".message").text(msg);
-    confirmBox.find(".yes").unbind().click(function() {
-        confirmBox.hide();
-     });
-     confirmBox.find(".yes").click(myYes);
-     confirmBox.show();
-  }
