08e37465d83bf369561f6981ec8bc00ef05510c6
doya986 <169078407+doya986@users.noreply.github.com>
2024-05-10 05:00:22 +0300

    Update account.js

--- script/account.js
+++ script/account.js
@@ -1,26 +1,46 @@
-let
-   tetrisScore=0;
-   BOScore=0;
-   XOWins=0;
-   SnakeScore=0;
-   SIScore=0;
+var tetrisScore=0;
+var newTetrisScore=parseInt(localStorage.getItem('tetrisScore'));
+var BOScore=0;
+var newBOScore=parseInt(localStorage.getItem('BOscore'));
+var XOStreak=0;
+var newXOStreak=0;
+var snakeScore=0;
+var newSnakeScore=parseInt(localStorage.getItem('snakeScore'));
+var SIScore=0;
+var newSIScore=parseInt(localStorage.getItem('SIscore'));
 
-function getTetrisScore(){
 
-    return tetrisScore;
+if(newBOScore>BOScore)
+        BOScore=newBOScore;
+
+if(newTetrisScore>tetrisScore)
+    tetrisScore=newTetrisScore;
+
+if(newXOStreak>XOStreak)
+    XOStreak=newXOStreak;
+
+if(newSnakeScore>snakeScore)
+    snakeScore=newSnakeScore;
+
+if(newSIScore>SIScore)
+    SIScore=newSIScore;
+
+window.onload= function (){
+
+    let p1=document.getElementById('tet');
+    p1.innerText=tetrisScore+" points";
+
+
+    let p2=document.getElementById('BO');
+    p2.innerText=BOScore+' points';
+
+    let p3=document.getElementById('tic');
+    p3.innerText=XOStreak+' points';
+
+    let p4=document.getElementById('snake');
+    p4.innerText=snakeScore+' points';
+
+    let p5=document.getElementById('space');
+    p5.innerText=SIScore+' points';
 }
-function setTetrisScore(score){
-    tetrisScore=9;
-}
-function getBOScore(){
-    return BOScore;
-}
-function getXOWins(){
-    return XOWins;
-}
-function getSnakeScore(){
-    return SnakeScore;
-}
-function getSIScore(){
-    return SIScore;
-}
+
