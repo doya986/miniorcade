7c29e8f5a9a94d84bfd88969998e808b8d2f1bf8
doya986 <169078407+doya986@users.noreply.github.com>
2024-05-10 20:53:09 +0300

    Update account.js

--- script/account.js
+++ script/account.js
@@ -4,7 +4,7 @@ var newTetrisScore=parseInt(localStorage.getItem('tetrisScore'));
 var BOScore=0;
 var newBOScore=parseInt(localStorage.getItem('BOscore'));
 var XOStreak=0;
-var newXOStreak=0;
+var newXOStreak=parseInt(localStorage.getItem('winStrteak'));
 var snakeScore=0;
 var newSnakeScore=parseInt(localStorage.getItem('snakeScore'));
 var SIScore=0;
@@ -28,20 +28,53 @@ if(newSIScore>SIScore)
 
 window.onload= function (){
 
-    let p1=document.getElementById('tet');
+    var p1=document.getElementById('tet');
     p1.innerText=tetrisScore+" points";
-
+    color("tet", tetrisScore);
 
     let p2=document.getElementById('BO');
     p2.innerText=BOScore+' points';
+    color("BO", BOScore);
 
     let p3=document.getElementById('tic');
-    p3.innerText=XOStreak+' points';
+    p3.innerText=XOStreak+' wins in a row';
+    color("tic", XOStreak);
 
     let p4=document.getElementById('snake');
     p4.innerText=snakeScore+' points';
+    color("snake", snakeScore);
 
-    let p5=document.getElementById('space');
+    var p5=document.getElementById('space');
     p5.innerText=SIScore+' points';
+    color("space", SIScore);
+
 }
 
+function color(i, score){
+
+    let p=document.getElementById(i);
+
+    if(score>=20000){
+            p.classList.add("gold");
+            return;
+    }        
+    else if (score>=10000){  
+            p.classList.add("prpl");
+            return;
+    }        
+    else if (score>=5000){
+        p.classList.add("blue");
+            return; 
+    }
+    else if(score>=2000){
+        p.classList.add("blue");
+        return;
+    }
+    else if(score>=0){
+        p.classList.add("gray");
+        return;
+    }
+    else
+        return;        
+
+}
